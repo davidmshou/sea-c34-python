b4cb7fbef57d1746bd39f739028b6ffc08e6a907
davidmshou <david.m.shou@gmail.com>
2015-03-27 08:39:10 -0700

    Step 1 complete.

--- students/DavidShoubridge/session04/dict_lab.py
+++ students/DavidShoubridge/session04/dict_lab.py
@@ -1,3 +1,24 @@
 new_dictionary = {"name": "Michael", "city": "Seattle", "cake": "Chocolate"}
 
 print(new_dictionary)
+
+del new_dictionary["cake"]
+
+print(new_dictionary)
+
+new_dictionary["fruit"] = "Mango"
+
+print(new_dictionary)
+
+for key in new_dictionary.iterkeys():
+    print key
+
+for value in new_dictionary.itervalues():
+    print value
+
+print(new_dictionary.get("cake"))
+
+
+for value in new_dictionary.values():
+    if value == "Mango":
+        print("We've got %s's." % (value))
