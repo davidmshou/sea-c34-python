1aa5f6d308e0415a2d0e7ea367508c9aae2afabc
davidmshou <david.m.shou@gmail.com>
2015-03-30 17:04:05 -0700

    Add creation of email.

--- students/DavidShoubridge/session04/mailroom_madness.py
+++ students/DavidShoubridge/session04/mailroom_madness.py
@@ -31,6 +31,7 @@ def name_prompt():
     """Prompt user for name of donor, or list of donors.
     """
     user_input = raw_input(u"Please enter donor's name, (l)ist, or (q)uit: ")
+    donor_tuple = tuple(donor_dict)
 
     if user_input.lower() == "l":
         print(donor_dict.keys())
@@ -38,14 +39,29 @@ def name_prompt():
     elif user_input.lower() == "q":
         sys.exit(0)
     else:
-        for key in donor_dict:
-            if user_input.lower() == key:
+        for key in donor_tuple:
+            if user_input.lower() == key.lower():
                 donation_amount(user_input)
-            elif user_input.lower() != key:
-                donor_dict[key].append(user_input)
-                donation_amount(user_input)
+                break
+            else:
+                if user_input.lower() != key.lower():
+                    donor_dict[user_input] = []
+                    donation_amount(user_input)
+                break
 
 
+def donation_amount(donor):
+    donation_input = raw_input(u"Enter donation amount: ")
+    donation_int = int(donation_input)
+
+    if donor in donor_dict:
+        donor_dict[donor].append(donation_int)
+        email_format(donor, donation_int)
+
+
+def email_format(donor, amount):
+    print(u"Hello, %s! Thank you for your donation of %i" % (donor, amount))
 
 if __name__ == "__main__":
-    select_operation()
+    while True:
+        select_operation()
