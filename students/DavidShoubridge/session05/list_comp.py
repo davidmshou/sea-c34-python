eb2f61209fda68152d97ad7a2ece694d12dd4fe8
davidmshou <david.m.shou@gmail.com>
2015-04-01 16:00:03 -0700

    Add count_evens function.

---
+++ students/DavidShoubridge/session05/list_comp.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+
+
+def count_evens(nums):
+    evens = [number for number in nums if number % 2 == 0]
+    return evens
+
+
+if __name__ == "__main__":
+    print(count_evens([2, 1, 2, 3, 4]))
+    print(count_evens([2, 2, 0]))
+    print(count_evens([1, 3, 5]))
