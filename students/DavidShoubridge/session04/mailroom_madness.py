b6815bb7449d9ab95e8596dc2403d5d9863a4042
davidmshou <david.m.shou@gmail.com>
2015-04-01 17:41:48 -0700

    Add title.

--- students/DavidShoubridge/session04/mailroom_madness.py
+++ students/DavidShoubridge/session04/mailroom_madness.py
@@ -21,9 +21,7 @@ def select_operation():
     """Request input from user, which will either create a report or send a \
     thank-you.
     """
-    user_input = safe_input(
-        u"Enter: (C)reate a report, (S)end thank you, or (Q)uit: "
-    )
+    user_input = safe_input(u"Enter: (C)reate a report, (S)end thank you, or (Q)uit: ")
 
     if user_input.lower() == u"c" or user_input.lower() == u"create a report":
         donor_list()
@@ -66,16 +64,18 @@ def donor_list():
         )
     print("\n\n")
 
-select_operation()
+    select_operation()
 
 
 def name_prompt():
     """
     Prompt user for name of donor, or list of donors.
     """
-    user_input = safe_input(u"Please enter Donor's name, (L)ist, (M)ain menu, or (Q)uit: ")
-    donor_tuple = tuple(donor_dict)
-
+    user_input = safe_input(
+        u"Please enter Donor's name, (L)ist, (M)ain menu, or (Q)uit: "
+    )
+    #donor_tuple = tuple(donor_dict)
+    #print(donor_tuple)
     if user_input.lower() == u"l" or user_input.lower() == u"list":
         print(donor_dict.keys())
         name_prompt()
@@ -86,10 +86,12 @@ def name_prompt():
     else:
         for key in donor_tuple:
             if user_input.lower() == key.lower():
+                print("Donor in database")
                 donation_amount(user_input)
                 break
             else:
                 if user_input.lower() != key.lower():
+                    print("Donor not in database")
                     donor_dict[user_input] = []
                     donation_amount(user_input)
                 break
@@ -119,7 +121,15 @@ def email_format(donor, amount):
     print(u"Hello, %s! Thank you for your donation of %f" % (donor, amount))
 
 if __name__ == "__main__":
-    print(u"Mailroom Madness")
+    print(
+        " __  __        __  __           _                     \n"
+        "|  \/  |      |  \/  |         | |                    \n"
+        "| \  / |______| \  / | __ _  __| |_ __   ___  ___ ___ \n"
+        "| |\/| |______| |\/| |/ _` |/ _` | '_ \ / _ \/ __/ __|\n"
+        "| |  | |      | |  | | (_| | (_| | | | |  __/\__ \__ \ \n"
+        "|_|  |_|      |_|  |_|\__,_|\__,_|_| |_|\___||___/___/\n"
+        "                                                      \n"
+    )
 
     while True:
         select_operation()
