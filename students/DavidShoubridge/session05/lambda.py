e1e21fc6f5633c5089bbc576f15df80894d96cdd
davidmshou <david.m.shou@gmail.com>
2015-04-03 15:27:13 -0700

    Add function builder function.

--- students/DavidShoubridge/session05/lambda.py
+++ students/DavidShoubridge/session05/lambda.py
@@ -1,3 +1,9 @@
 def function_builder(n):
-    my_list = []
-    
+
+    return [lambda x, c=i: x + c for i in range(n)]
+
+
+if __name__ == "__main__":
+    the_list = function_builder(4)
+    assert(the_list[0](2)) == 2
+    assert(the_list[1](2)) == 3
