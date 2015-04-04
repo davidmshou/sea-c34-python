bfc337c2cbb45b71db78434661bacf09ef089190
davidmshou <david.m.shou@gmail.com>
2015-04-03 18:12:28 -0700

    Beginning work.

--- students/DavidShoubridge/session06/html_render.py
+++ students/DavidShoubridge/session06/html_render.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 
 """
 Python class example.
@@ -7,5 +9,17 @@ Python class example.
 
 # The start of it all:
 # Fill it all in here.
+
+
 class Element(object):
-    pass
+
+    tag_name = u""
+    indentation = u"    "
+
+    def __init__(self, content):
+        self.content = content
+
+    def append(self, content):
+        pass
+
+    def render(self, file_out, ind=""):
