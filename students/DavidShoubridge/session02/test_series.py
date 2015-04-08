0c4576d04dbe42bc487d41fc0f299800ec6d33e0
davidmshou <david.m.shou@gmail.com>
2015-04-08 16:30:44 -0700

    Add test for series.py.

---
+++ students/DavidShoubridge/session02/test_series.py
@@ -0,0 +1,13 @@
+import series
+
+
+def test_fibonacci():
+    assert(series.fibonacci(1)) == 1
+    assert(series.fibonacci(5)) == 5
+    assert(series.fibonacci(7)) == 13
+
+
+def test_lucas():
+    assert(series.lucas(0)) == 2
+    assert(series.lucas(3)) == 4
+    assert(series.lucas(7)) == 29
