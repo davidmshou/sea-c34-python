3462db60fefa39b9265574b48dc7eb064a129fa0
davidmshou <david.m.shou@gmail.com>
2015-04-08 17:46:45 -0700

    Fix form for asserts.

--- students/DavidShoubridge/session02/test_series.py
+++ students/DavidShoubridge/session02/test_series.py
@@ -2,12 +2,12 @@ import series
 
 
 def test_fibonacci():
-    assert(series.fibonacci(1)) == 1
-    assert(series.fibonacci(5)) == 5
-    assert(series.fibonacci(7)) == 13
+    assert(series.fibonacci(1) == 1)
+    assert(series.fibonacci(5) == 5)
+    assert(series.fibonacci(7) == 13)
 
 
 def test_lucas():
-    assert(series.lucas(0)) == 2
-    assert(series.lucas(3)) == 4
-    assert(series.lucas(7)) == 29
+    assert(series.lucas(0) == 2)
+    assert(series.lucas(3) == 4)
+    assert(series.lucas(7) == 29)
